53a08608d3ea1dfc5d89fd4c606a1cef5abc9c49
Björn van der Haas <73842831+B-Haas@users.noreply.github.com>
2022-10-15 07:11:30 +0200

    NEAT with custom dynamic changes

--- NEAT_specialist.py
+++ NEAT_specialist.py
@@ -50,18 +50,71 @@ class NEAT_Spealist():
                                     neat.DefaultStagnation,
                                     neat_config)
 
-        # sets up the population
+        # generation divider for dynamic tuning
+        gsec1 = 10
+        gsec2 = 5
+        gsec3 = (10+(self.gens-25))
+        if self.gens < 25:
+            if self.gens > 14:
+                gsec1 = 10
+                gsec2 = 5
+                gsec3 = (self.gens-15)
+            elif self.gens < 4:  # with the current divide less than 4 would cause issues with retaining pops
+                sys.exit("Please pick a generation size of 4 or more.")
+            else:
+                gsec1 = 1
+                gsec2 = 1
+                gsec3 = (self.gens-2)
+
+        # sets up the starting population
         pop = neat.Population(config)
         
         pop.add_reporter(neat.StdOutReporter(True))
         s = neat.StatisticsReporter()
         pop.add_reporter(s)
-        pop.add_reporter(neat.Checkpointer(self.gens))
+        # pop.add_reporter(neat.Checkpointer(self.gens))  # Checkpoints for recovey, inconvenient when running multiple
         # our reporter, to report mean/max fitness for each generation
         pop.add_reporter(generation_reporter(self.logpath))
 
-        # Run the algorithm
-        winner = pop.run(self.genome_evaluation, self.gens)
+        # Run first segment of the algorithm
+        pop.run(self.genome_evaluation, gsec1)
+
+        # Run second segment of the algorithm
+        neat_dir = os.path.dirname(__file__)
+        neat_path2 = os.path.join(neat_dir, "NEAT-config2.txt")
+
+        config2 = neat.config.Config(neat.DefaultGenome,
+                                    neat.DefaultReproduction,
+                                    neat.DefaultSpeciesSet,
+                                    neat.DefaultStagnation,
+                                    neat_path2)
+
+        pop2 = neat.Population(config2, initial_state=(pop.population, pop.species, pop.generation))
+
+        pop2.add_reporter(neat.StdOutReporter(True))
+        pop2.add_reporter(s)
+        # pop2.add_reporter(neat.Checkpointer(self.gens))
+        pop2.add_reporter(generation_reporter(self.logpath))
+
+        pop2.run(self.genome_evaluation, gsec2)
+
+        # Run third segment of the algorithm
+        neat_path3 = os.path.join(neat_dir, "NEAT-config3.txt")
+
+        config3 = neat.config.Config(neat.DefaultGenome,
+                                     neat.DefaultReproduction,
+                                     neat.DefaultSpeciesSet,
+                                     neat.DefaultStagnation,
+                                     neat_path3)
+
+        pop3 = neat.Population(config3, initial_state=(pop2.population, pop2.species, pop2.generation))
+
+        pop3.add_reporter(neat.StdOutReporter(True))
+        pop3.add_reporter(s)
+        # pop2.add_reporter(neat.Checkpointer(self.gens))
+        pop3.add_reporter(generation_reporter(self.logpath))
+
+        winner = pop3.run(self.genome_evaluation, gsec3)
         
         # save the winner
         with open(self.picklepath, "wb") as f:
